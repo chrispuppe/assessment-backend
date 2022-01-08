653986ed2e21f2deeb20f01aef74eec27eb4b59b
Chris Puppe <chrispuppe@gmail.com>
2022-01-08 13:33:32 -0800

    Code refactor and fortune button

--- server/index.js
+++ server/index.js
@@ -8,6 +8,13 @@ app.use(cors());
 
 app.use(express.json()); // When we want to be able to accept JSON.
 
+function randomReply(res, replyList) {
+  let randomIndex = Math.floor(Math.random() * replyList.length);
+  let randomMessage = replyList[randomIndex];
+
+  res.status(200).send(randomMessage);
+}
+
 app.get("/api/compliment", (req, res) => {
   const compliments = ["Gee, you're a smart cookie!",
 					 "Cool shirt!",
@@ -15,10 +22,24 @@ app.get("/api/compliment", (req, res) => {
   ];
 
   // choose random compliment
-  let randomIndex = Math.floor(Math.random() * compliments.length);
-  let randomCompliment = compliments[randomIndex];
+  // let randomIndex = Math.floor(Math.random() * compliments.length);
+  // let randomCompliment = compliments[randomIndex];
 
-  res.status(200).send(randomCompliment);
+  // res.status(200).send(randomCompliment);
+  randomReply(res, compliments)
+  
+});
+
+app.get("/api/fortune", (req, res) => {
+  const fortunes = ["Courtesy begins in the home.",
+					 "Don’t just spend time. Invest it.",
+					 "Don’t just think, act!",
+           "Now is a good time to sell stock.",
+           "Pennies from heaven find their way to your doorstep this year!",
+  ];
+
+  // choose random compliment
+  randomReply(res, fortunes)
   
 });
 
